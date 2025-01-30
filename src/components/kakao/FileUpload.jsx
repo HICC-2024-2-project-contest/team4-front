aabd4c8f6e0eac7d24ffe8967d816255a2c33e4a
YangYoonseo <yys0847@naver.com>
2025-01-30 23:39:59 +0900

    Feat: 카카오톡 파일 업로드 수정 카카오톡 input 타입을 file로 수정한다 Resolves:#3

--- src/components/kakao/FileUpload.jsx
+++ src/components/kakao/FileUpload.jsx
@@ -1,32 +1,43 @@
 import { useState } from "react";
+import axios from "axios";
 import Button from "../common/Button";
 import { useNavigate } from "react-router-dom";
 import "../../styles/kakao/FileUpload.css";
+import arrow from "../../assets/img_kakao/arrow.svg";
 
 const FileUpload = () => {
   const nav = useNavigate();
-  const [upload, setUpload] = useState(false);
+  const [file, setFile] = useState(null);
+
+  const handleFilesChange = (e) => {
+    setFile(e.target.files);
+  };
+
+  const handleUPload = async () => {
+    if (file == null) {
+      alert("파일을 업로드해야 진행할 수 있습니다!");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", file);
+    console.log(file);
+    nav("/kakao_talk3");
+  };
+
   return (
     <div className="blackBox">
       <div className="FileUpload">
-        <input
-          type="checkbox"
-          checked={upload}
-          onChange={() => setUpload(!upload)}
-        />
+        <input type="file" onChange={handleFilesChange} id="file-upload" />
+        <label htmlFor="file-upload"></label>
+        <img src={arrow} alt="Arrow Icon" className="arrow-icon" />
         <h6>파일 업로드</h6>
         <p>움직이는 대화 상자를 클릭해 도움을 받아보세요!</p>
         <Button
           text={"다음"}
           type={"white"}
-          onClick={() => {
-            if (!upload) {
-              alert("파일을 업로드해야 진행할 수 있습니다!");
-              return;
-            }
-            nav("/kakao_talk3");
-          }}
-          disabled={!upload}
+          onClick={handleUPload}
+          disabled={file == null}
         />
       </div>
     </div>
