129d2e28422f76d2f468fd3e76379063bad4c5e2
MinjaeMj <85chlalswo@naver.com>
2025-01-31 17:49:09 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/common/Header";
+import Onboarding from "./components/common/pages/Onboarding";
+import Questionnaire from "./components/common/pages/Questionaire";
 
 function App() {
-  return <div>presenTalk</div>;
+  console.log("✅ App 컴포넌트 렌더링됨!");
+  return (
+      <div className="AppContainer">
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Onboarding />} />
+            <Route path="/questionnaire" element={<Questionnaire />} />
+          </Routes>
+        </div>
+      </div>
+  );
 }
 
 export default App;
