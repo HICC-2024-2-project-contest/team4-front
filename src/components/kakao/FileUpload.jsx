0576d373ae1c5745166927b7b9908dfd9e221a84
YangYoonseo <yys0847@naver.com>
2025-02-05 16:53:12 +0900

    Feat: 버튼 애니메이션 구현 파일 업로드 버튼 애니메이션 효과 @keyframes 이용해 구현 Fixes:#10

--- src/components/kakao/FileUpload.jsx
+++ src/components/kakao/FileUpload.jsx
@@ -1,19 +1,31 @@
 import { useState } from "react";
-import axios from "axios";
 import Button from "../common/Button";
 import { useNavigate } from "react-router-dom";
 import "../../styles/kakao/FileUpload.css";
 import arrow from "../../assets/img_kakao/arrow.svg";
+import check from "../../assets/img_kakao/check.svg";
 
 const FileUpload = () => {
   const nav = useNavigate();
   const [file, setFile] = useState(null);
+  const [stage, setStage] = useState("initial");
+  // initial -> exploding -> collapsing -> completed
 
   const handleFilesChange = (e) => {
-    setFile(e.target.files);
+    if (e.target.files.length > 0) {
+      setFile(e.target.files[0]);
+      setStage("exploding");
+
+      setTimeout(() => {
+        setStage("collapsing");
+        setTimeout(() => {
+          setStage("completed");
+        }, 3500);
+      }, 1000);
+    }
   };
 
-  const handleUPload = async () => {
+  const handleUpload = async () => {
     if (file == null) {
       alert("파일을 업로드해야 진행할 수 있습니다!");
       return;
@@ -28,16 +40,31 @@ const FileUpload = () => {
   return (
     <div className="blackBox">
       <div className="FileUpload">
+        <div className={`gray_box gray_box_${stage}`}></div>
         <input type="file" onChange={handleFilesChange} id="file-upload" />
         <label htmlFor="file-upload">
-          <img src={arrow} alt="Arrow Icon" className="arrow-icon" />
+          {stage === "completed" ? (
+            <img src={check} alt="Check Icon" className="check-icon" />
+          ) : (
+            <img src={arrow} alt="Arrow Icon" className="arrow-icon" />
+          )}
         </label>
+        {/* 애니메이션 효과를 위한 동적 원 생성 */}
+
+        {(stage === "exploding" || stage === "collapsing") && (
+          <div className="animation-container">
+            {[...Array(12)].map((_, i) => (
+              <div key={i} className={`particle particle_${stage}`}></div>
+            ))}
+          </div>
+        )}
+
         <h6>파일 업로드</h6>
         <p>움직이는 대화 상자를 클릭해 도움을 받아보세요!</p>
         <Button
           text={"다음"}
           type={file !== null ? "white" : "gray"}
-          onClick={handleUPload}
+          onClick={handleUpload}
           disabled={file == null}
         />
       </div>
