3138bd16f787ca1f0312ff3dfcc1cc307b96b094
YangYoonseo <yys0847@naver.com>
2025-02-10 18:35:22 +0900

    Feat: 로딩창 캐릭터 추가 로딩중에서 캐릭터 이미지 추가 Fixes:#10

--- src/components/result/Loading.jsx
+++ src/components/result/Loading.jsx
@@ -1,6 +1,21 @@
 import "../../styles/result/Loading.css";
+import character1 from "../../assets/img_result/character1.svg";
+import character2 from "../../assets/img_result/character2.svg";
+import ground from "../../assets/img_result/ground.svg";
+
+import { useState, useEffect } from "react";
 
 const Loading = () => {
+  const [showFirstCharacter, setShowFirstCharacter] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setShowFirstCharacter((prev) => !prev);
+    }, 1000); // 1초마다 실행
+
+    return () => clearInterval(interval); // 컴포넌트 언마운트 시 정리
+  }, []);
+
   return (
     <div className="Loading">
       <h5>선물 고르는 중...</h5>
@@ -10,6 +25,12 @@ const Loading = () => {
         <br />
         선물을 골라보세요!
       </p>
+      <img
+        src={showFirstCharacter ? character1 : character2}
+        alt="마스코트"
+        className="character"
+      />
+      <img src={ground} alt="땅" className="ground" />
     </div>
   );
 };
