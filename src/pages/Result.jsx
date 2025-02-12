8587ec160700855015972c8620f18361cbfa1647
YangYoonseo <yys0847@naver.com>
2025-02-12 11:24:28 +0900

    Chore: result height, overscroll 관련 수정

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,6 +1,7 @@
 import Header from "../components/common/Header";
 import Ment2 from "../components/result/Ment2";
 import Inventory from "../components/result/Inventory";
+import "../styles/result/Result.css";
 
 const Result = () => {
   return (
