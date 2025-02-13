e574ebaabbe258a69d8350b7aeaff2f42515ab61
YangYoonseo <yys0847@naver.com>
2025-02-13 13:37:43 +0900

    Fix: 반응형 수정, meta 태그 추가

--- src/pages/KakaoTalkAgree.jsx
+++ src/pages/KakaoTalkAgree.jsx
@@ -2,13 +2,8 @@ import Header from "../components/common/Header";
 import Agree from "../components/kakao/Agree";
 import Ment from "../components/kakao/ment";
 import SpeechBubble from "../components/kakao/SpeechBubble";
-import useUser from "../hooks/UseUser";
 
 const KakaoTalkAgree = () => {
-
-  const { userData } = useUser(); 
-  console.log(userData); {/*유저 데이터 확인용*/}
-
   return (
     <div className="KakaoTalk KakaoTalkAgree">
       <Header />
